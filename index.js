ad6c11602ff1a1df754b1ad1485b38c1156b7510
Yara Alkassar <yara.sarmad@yahoo.com>
2020-05-28 10:05:15 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 function fetchBooks() {
-  return fetch('https://anapioficeandfire.com/api/books')
+  return fetch('https://anapioficeandfire.com/api/characters/583')
           .then(response => response.json())
           .then(json => renderBooks(json));
 }
